2967a559ecd41576d34175d04ecf1dc316201113
Bruno Parga <brunoparga@gmail.com>
2024-01-16 20:22:04 +0200

    Actually insert cards into Anki

--- index.js
+++ index.js
@@ -13,6 +13,46 @@ const doc = new Sheet(credentials.file_id, jwt);
 await doc.loadInfo();
 const sheet = doc.sheetsByIndex[0];
 const rows = await sheet.getRows();
-for (let row of rows) {
-  console.log(`${row.get('Sõna')}: ${row.get('Inglise')}`);
+
+const notes = rows.map((row) => ({
+  deckName: 'Fesf',
+  modelName: 'Basic (and reversed card)',
+  fields: {
+    Front: `🇪🇪 ${row.get('Sõna')}`,
+    Back: `🏴󠁧󠁢󠁥󠁮󠁧󠁿 ${row.get('Inglise')}`,
+  },
+  options: {
+    allowDuplicate: false,
+    duplicateScope: 'deck',
+    duplicateScopeOptions: {
+      checkAllModels: true,
+    },
+  },
+}));
+
+const options = {
+  method: 'POST',
+  body: JSON.stringify({
+    action: 'addNotes',
+    version: 6,
+    params: { notes },
+  }),
+};
+
+const ankiURL = 'http://127.0.0.1:8765';
+
+const response = await (await fetch(ankiURL, options)).json();
+
+if (response.error) {
+  console.log('The program has encountered an error.');
+} else {
+  const countNonNull = (acc, elt) => (elt ? acc + 1 : acc);
+  const inserted = response.result.reduce(countNonNull, 0);
+  const duplicates =
+    inserted == rows.length ? '' : 'Since duplicates are skipped, ';
+  console.log(
+    `The spreadsheet "${doc.title}" has ${rows.length} rows. ${duplicates}${
+      inserted ? inserted * 2 : 'no'
+    } cards were inserted in the deck. ${inserted ? 'Have fun!' : ''}`
+  );
 }
